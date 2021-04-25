2b583ce9c3feda083d493cc566c5b22d3fb9a6c4
evanwill <evanpeterw@gmail.com>
2021-04-24 20:17:00 -0700

    rearrange ifs in lunr search reduces store and display, avoids errors

--- assets/js/lunr-store.js
+++ assets/js/lunr-store.js
@@ -5,6 +5,10 @@
 {%- assign fields = site.data.config-search -%}
 var store = [ 
 {%- for item in items -%} 
-{ "id": {{ item.objectid | jsonify }}, {% for f in fields %}{{ f.field | jsonify }}: {% if item[f.field] %}{{ item[f.field] | normalize_whitespace | replace: '""','"' | jsonify }}{% else %}"none"{% endif %}{% unless forloop.last %},{% endunless %}{% endfor %} }{%- unless forloop.last -%},{%- endunless -%}
+{  
+{% for f in fields %}{% if item[f.field] %}{{ f.field | jsonify }}: {{ item[f.field] | normalize_whitespace | replace: '""','"' | jsonify }},{% endif %}{% endfor %} 
+"id": {{ item.objectid | jsonify }}
+
+}{%- unless forloop.last -%},{%- endunless -%}
 {%- endfor -%}
 ];
